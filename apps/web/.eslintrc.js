3e219dccb2663b1da558b75f0f5e50516b582770
cobocho <rlaalsrb1111@naver.com>
2024-04-25 08:50:56 +0900

    chore: `turborepo` 마이그레이션
    Related on: #1

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,9 +1,5 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
+  extends: ['@vook-client/eslint-config/next.js'],
 };
--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,35 +1,6 @@
-const { resolve } = require("node:path");
-
-const project = resolve(process.cwd(), "tsconfig.json");
-
-/** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "prettier",
-    require.resolve("@vercel/style-guide/eslint/next"),
-    "eslint-config-turbo",
-  ],
-  globals: {
-    React: true,
-    JSX: true,
+  extends: ['next/core-web-vitals', '@vook-client/eslint-config/react.js'],
+  rules: {
+    '@next/next/no-page-custom-font': 'off',
   },
-  env: {
-    node: true,
-    browser: true,
-  },
-  plugins: ["only-warn"],
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project,
-      },
-    },
-  },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-  ],
-  overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
 };
