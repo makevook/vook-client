0bc0acda6029ecc68f73aaa56ca61ce761fbd474
cobocho <rlaalsrb1111@naver.com>
2024-04-26 02:40:31 +0900

    chore: 패키지명 오타 수정

--- apps/chrome-extension/vite.config.ts
+++ apps/chrome-extension/vite.config.ts
@@ -2,7 +2,7 @@ import { crx } from '@crxjs/vite-plugin'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-import manifest from './src/manifest'
+import manifest from './src/manifest.js'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
