9d6b7fe6b495f3940fcb27e512614419c273651e
Seungyeop Lee <xbeast7@gmail.com>
2024-05-14 17:50:34 +0900

    [Feature] staging 환경 배포 구정 (#5)
    * chore: 배포를 위한 Dockerfile 추가
    
    * chore: dagger 추가 및 빌드 스크립트 추가
    
    * chore: dagger 배포 스크립트 추가
    
    * chore: staging 환경 배포용 workflow 추가

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -8,6 +8,7 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  output: 'standalone',
 }
 
 export default withVanillaExtract(nextConfig)
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,12 +4,16 @@ import { Button } from '@vook-client/ui'
 
 import { TestComponent } from '@/components/TestComponent'
 
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || 'https://dev.vook-api.seungyeop-lee.com'
+
 const Home = () => {
   return (
     <main>
       Hello world!
       <TestComponent />
       <Button>Button</Button>
+      <h1>API_URL: {API_URL}</h1>
     </main>
   )
 }
