54035a0589f10873621f36b1ac8c7b2ab867b961
seungyeop-lee <xbeast7@gmail.com>
2024-05-14 15:26:22 +0900

    chore: dagger 배포 스크립트 추가

--- cicd/dagger/main.go
+++ cicd/dagger/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"strings"
+)
+
 type VookClient struct{}
 
 func (v *VookClient) BuildClientImage(
@@ -16,4 +22,91 @@ func (v *VookClient) BuildClientImage(
 			Name: "web-client",
 		}).
 		File("web-client_linux_arm64.tar")
+}
+
+func (v *VookClient) SendImage(
+	ctx context.Context,
+	sshDest *Secret,
+	sshKey *Secret,
+	path string,
+	imageTar *File,
+) error {
+	sshDestText, err := sshDest.Plaintext(ctx)
+	if err != nil {
+		return err
+	}
+
+	_, err = dag.Scp().
+		Config(strings.TrimSpace(sshDestText)).
+		WithIdentityFile(sshKey).
+		FileToRemote(imageTar, ScpCommanderFileToRemoteOpts{
+			Target: path,
+		}).
+		Sync(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (v *VookClient) Apply(
+	ctx context.Context,
+	destination *Secret,
+	sshKey *Secret,
+	imageTar *File,
+	path string,
+	version string,
+	command string,
+) error {
+	destinationText, err := destination.Plaintext(ctx)
+	if err != nil {
+		return err
+	}
+
+	filename, err := imageTar.Name(ctx)
+	if err != nil {
+		return err
+	}
+
+	_, err = dag.SSH().
+		Config(strings.TrimSpace(destinationText)).
+		WithIdentityFile(sshKey).
+		Command(
+			fmt.Sprintf(`
+cd %s
+CLIENT_FILENAME=%s CLIENT_VERSION=%s %s
+`, path, filename, version, command),
+		).
+		Sync(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (v *VookClient) Deploy(
+	ctx context.Context,
+	sourceDir *Directory,
+	dockerfile *File,
+	sshDest *Secret,
+	sshKey *Secret,
+	targetPath string,
+	version string,
+	command string,
+) error {
+	imageTar := v.BuildClientImage(sourceDir, dockerfile)
+
+	err := v.SendImage(ctx, sshDest, sshKey, targetPath, imageTar)
+	if err != nil {
+		return err
+	}
+
+	err = v.Apply(ctx, sshDest, sshKey, imageTar, targetPath, version, command)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
