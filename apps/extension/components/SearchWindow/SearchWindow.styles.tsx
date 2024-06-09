e759396fcbf78c4f2a75ec27b6003a374e08c2a4
Kim Min-gyu <99083803+cobocho@users.noreply.github.com>
2024-06-09 22:47:50 +0900

    refactor: 미사용 컴포넌트 제거

--- apps/extension/components/SearchWindow/SearchWindow.styles.tsx
+++ apps/extension/components/SearchWindow/SearchWindow.styles.tsx
@@ -44,53 +44,6 @@ export const SearchWindowBox = styled.div<{ position: Position }>`
   }
 `
 
-export const SearchWindowHeader = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  position: relative;
-
-  margin-bottom: 16px;
-
-  .term-header {
-    display: flex;
-    align-items: center;
-    width: 100%;
-
-    img {
-      width: 22px;
-      height: 22px;
-      margin-right: 6px;
-      transform: translateY(-2px);
-    }
-
-    p {
-      font-size: 14px;
-      font-weight: 700;
-      line-height: 1.2;
-
-      &.query {
-        display: block;
-        max-width: 50%;
-
-        text-overflow: ellipsis;
-        overflow: hidden;
-        white-space: nowrap;
-      }
-
-      &.tail {
-        margin-left: 3px;
-      }
-    }
-  }
-
-  button {
-    position: absolute;
-    top: 0;
-    right: 0;
-  }
-`
-
 export const SearchWindowLink = styled.div`
   display: flex;
   justify-content: flex-end;
