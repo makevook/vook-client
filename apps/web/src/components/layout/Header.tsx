548b4ef8bd7514f1593ccce1e9271c8e418505da
hin6150 <hin6150@gmail.com>
2024-06-10 11:43:09 +0900

    feat: download 버튼 렌더링 로직 수정

--- apps/web/src/components/layout/Header.tsx
+++ apps/web/src/components/layout/Header.tsx
@@ -4,25 +4,55 @@ import { Button } from '@vook-client/design-system'
 
 import { Logo } from '../common'
 
-import { displayNone, header, headerContainer, inner } from './Layout.css'
+import { header, headerContainer, inner } from './Layout.css'
+
+enum Browser {
+  Chrome,
+  Safari,
+  Edge,
+  Firefox,
+  Other,
+}
+
+const detectBrowser = (): Browser => {
+  const userAgent = navigator.userAgent
+
+  if (/Edg/.test(userAgent)) {
+    return Browser.Edge
+  } else if (/Chrome/.test(userAgent) && /Google Inc/.test(navigator.vendor)) {
+    return Browser.Chrome
+  } else if (/Safari/.test(userAgent) && !/Chrome/.test(userAgent)) {
+    return Browser.Safari
+  } else if (/Firefox/.test(userAgent)) {
+    return Browser.Firefox
+  } else {
+    return Browser.Other
+  }
+}
 
 export const Header = () => {
-  const [isChrome, setIsChrome] = useState(false)
+  const [browser, setBrowser] = useState<Browser | null>(null)
 
   useEffect(() => {
-    setIsChrome(
-      /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor),
-    )
+    setBrowser(detectBrowser())
   }, [])
+
+  const renderBrowserSpecificComponent = () => {
+    switch (browser) {
+      case Browser.Chrome:
+        return <Button size="small">Download</Button>
+      // 필요한 경우 다른 브라우저에 대한 컴포넌트 추가
+      default:
+        return null
+    }
+  }
 
   return (
     <div className={header}>
       <div className={inner}>
         <div className={headerContainer}>
           <Logo />
-          <div id="chrome-only-element" className={isChrome ? '' : displayNone}>
-            <Button size="small">Download</Button>
-          </div>
+          {renderBrowserSpecificComponent()}
         </div>
       </div>
     </div>
