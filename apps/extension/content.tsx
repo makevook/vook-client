48b5dce176981fac7d211ce8e468c5bf319f4d23
Kim Min-gyu <99083803+cobocho@users.noreply.github.com>
2024-06-10 13:33:33 +0900

    fix: `@vook-client/extension` 번들러 수정사항 적용
    Related on: #16

--- apps/extension/content.tsx
+++ apps/extension/content.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line import/no-unresolved
+import styleText from 'data-text:@vook-client/design-system/style.css'
 import createCache from '@emotion/cache'
 import { CacheProvider, Global } from '@emotion/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -16,7 +18,10 @@ const styleCache = createCache({
   container: styleElement,
 })
 
-export const getStyle = () => styleElement
+export const getStyle = () => {
+  styleElement.textContent = styleText.replaceAll(':root', ':host')
+  return styleElement
+}
 
 const queryClient = new QueryClient({
   defaultOptions: {
