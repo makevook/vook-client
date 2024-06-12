b32c5a0bc1559ed48e0fe557413913d49463b888
Kim Min-gyu <99083803+cobocho@users.noreply.github.com>
2024-06-13 02:25:16 +0900

    fix: 에러 발생하는 스토리 빌드 제외

--- apps/extension/components/SearchWindow/SearchWindow.stories.tsx
+++ apps/extension/components/SearchWindow/SearchWindow.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { SearchWindow } from './SearchWindow'
 
@@ -13,7 +14,9 @@ const meta = {
           height: '600px',
         }}
       >
-        <Story />
+        <QueryClientProvider client={new QueryClient()}>
+          <Story />
+        </QueryClientProvider>
       </div>
     ),
   ],
