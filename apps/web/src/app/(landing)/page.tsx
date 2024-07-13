597879b6dab158a19524a6d6b064d10347e8c474
hin6150 <hin6150@gmail.com>
2024-07-13 17:12:26 +0900

    feat: 아이콘 컴포넌트 제작

--- apps/web/src/app/(landing)/page.tsx
+++ apps/web/src/app/(landing)/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import { IconBox } from './_component'
+
 const LandingPage = () => {
-  return <div>랜딩</div>
+  return (
+    <div style={{ marginTop: '200px' }}>
+      <IconBox name="notion" />
+      <IconBox name="jira" />
+      <IconBox name="excel" />
+    </div>
+  )
 }
 
 export default LandingPage
