08a12656666aa7c98a7d79278cd00d21a02ce1fa
Kim Min-gyu <rlaalsrb1111@naver.com>
2024-07-18 21:27:01 +0900

    hotfix: 타입 관련 에러 수정

--- setupTests.ts
+++ setupTests.ts
@@ -1,9 +1,12 @@
 import '@testing-library/jest-dom'
 
+// @ts-ignore
 import { afterEach } from 'vitest'
 
+// @ts-ignore
 import { cleanup } from '@testing-library/react'
 
+// @ts-ignore
 vi.mock('next/font/local', () => {
   return {
     default: () => {},
